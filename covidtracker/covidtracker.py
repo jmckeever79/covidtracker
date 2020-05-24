d201066cf002e6280946c5ce67643d343c334e6a
jmckeever79 <jj@jjmckeever.com>
2020-05-23 22:06:41 -0400

    expanding covid library

--- covidtracker/covidtracker.py
+++ covidtracker/covidtracker.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 from datetime import datetime
 
@@ -16,8 +17,11 @@ class CovidTracker(object):
             self.mostrecentdate = self.df['date'].max()
         return self.mostrecentdate
 
-    def getdateframe(self, date):
-        return self.df.loc[self.df['date']==date]
+    def getdateframe(self, date, indexed=False):
+        temp = self.df.loc[self.df['date']==date]
+        if indexed:
+            temp = temp.set_index(['state', 'county'])
+        return temp
 
     def getstateframe(self, state):
         temp = self.df.loc[self.df['state']==state]
@@ -30,3 +34,21 @@ class CovidTracker(object):
     def getcounties(self, state):
         sf = self.getstateframe(state)
         return set(sf['county'])
+
+    def _set_index(self, frame):
+        return frame.set_index(['state', 'county'])
+
+    def getperioddifference(self, enddate, days=1, stat='cases', pct_change=False):
+        endframe = self.getdateframe(enddate, indexed=False)
+        ts = pd.Timestamp(enddate)
+        offset = '1 day' if days==1 else '{0} days'.format(days)
+        startdate = str((ts - pd.Timedelta(offset)).date())
+        startframe = self.getdateframe(startdate, indexed=False)
+
+        endframe = self._set_index(endframe)
+        startframe = self._set_index(startframe)
+
+        diff = endframe[stat]-startframe[stat]
+        if pct_change:
+            diff = diff/startframe[stat]
+        return diff
